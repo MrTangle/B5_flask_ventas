9582f09da7bdc4399f15e58b79a42bdd19384215
MrTangle <alexibanez86@gmail.com>
2023-03-22 17:48:03 +0100

    Adding tests to app

---
+++ test_base.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_testing import TestCase
+import run as myApp
+
+class TestFlaskBase(TestCase):
+    def create_app(self):
+        self.app = myApp.app
+        return myApp.app
+        
+        self.app = app
+        return app
+    
+    def setUp(self):
+        self.client = self.app.test_client()
+        self.client.testing = True
+
+    def tearDown(self):
+        pass
