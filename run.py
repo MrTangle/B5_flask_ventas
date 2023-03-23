b0ffae85e57dba669af009b74d16d92d579fed53
MrTangle <alexibanez86@gmail.com>
2023-03-23 19:54:50 +0100

    json paises

--- run.py
+++ run.py
@@ -1,18 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import csv
 # Crear la instancia de la aplicación Flask
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     fVentas = open("./sales10.csv", "r")
     csvreader = csv.reader(fVentas, delimiter=",")
 
-    registros = []
     d = {}
     for linea in csvreader:
-        registros.append(linea)
         if linea[0] in d:
             d[linea[0]]["ingresos"] += float(linea[10])
             d[linea[0]]["beneficios"] += float(linea[12])
@@ -21,15 +18,22 @@ def index():
                 d[linea[0]] = {"ingresos": float(linea[10]), "beneficios": float(linea[12])}
 
     return render_template("region.html", ventas=d)
-    
-'''
-    <mientras haya registros>
 
-        registro = leerFichero
-        registro. separar.por comas
-        procesar ventas totales y beneficios totales
-    
-    Montar respuesta
-    devolver respuesta
 
-'''
+@app.route("/paises")
+def paises():
+    region_name = request.values["region"]
+
+    fVentas = open("./sales10.csv", "r")
+    csvreader = csv.reader(fVentas, delimiter=",")
+    d = {}
+    for linea in csvreader:
+        if linea[0] == region_name:
+            if linea[1] in d:
+                d[linea[1]]["ingresos"] += float(linea[10])
+                d[linea[1]]["beneficios"] += float(linea[12])
+            else:
+                d[linea[1]] = {"ingresos": float(linea[10]), "beneficios": float(linea[12])}
+
+
+    return d
